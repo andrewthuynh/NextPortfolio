7373a31073137a8b85e94c68b41a8fa19e33eeb3
Andrew Huynh <andrewtaylorhuynh@gmail.com>
2019-06-24 19:03:47 -0400

    padding

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Container } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
     intro: {
-        backgroundColor: "#bee3f8"
+        backgroundColor: "#667eea",
+        margin: -7,
+        padding: 0
     },
     introText: {
-        margin: 100
+        padding: 50,
+        color: '#4fd1c5'
     }
 });
 
@@ -28,18 +31,12 @@ class Index extends Component {
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 </Head>
-                <Grid
-                    justify='center'
-                    direction='row'
-                    alignItems='center'
-                >
-                    <div className={classes.intro}>
-                        <div className={classes.introText}>
-                            <Typography variant='h5'>Hello.</Typography>
-                            <Typography variant='h2'>Welcome to my new website.</Typography>
-                        </div>
+                <section className={classes.intro}>
+                    <div className={classes.introText}>
+                        <Typography variant='h5' color="inherit" align="center">Hello!</Typography>
+                        <Typography variant='h2' color="inherit" align="center">Welcome to my new website.</Typography>
                     </div>
-                </Grid>
+                </section>
             </Fragment>
         )
     }
