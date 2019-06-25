774ae1ea7151604997839feb33f4d77fd5f30581
Andrew Huynh <andrewtaylorhuynh@gmail.com>
2019-06-24 23:22:25 -0400

    nav and sections

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,43 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { Typography, Grid, Container } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import NavButtons from '../components/NavButtons';
 
 const styles = theme => ({
     intro: {
-        backgroundColor: "#667eea",
-        margin: -7,
+        //backgroundColor: "#667eea",
+        backgroundColor: '#F4796B',
+        margin: -10,
         padding: 0
     },
     introText: {
-        padding: 50,
-        color: '#4fd1c5'
+        paddingTop: 100,
+        paddingLeft: 50,
+        paddingRight: 50,
+        paddingBottom: 50,
+        color: 'white'
+    },
+    introText2: {
+        paddingLeft: 50,
+        paddingRight: 50,
+        paddingBottom: 100,
+        color: 'white'
+    },
+    section: {
+        margin: -10,
+        padding: 0,
+    },
+    sectionText: {
+        paddingLeft: '20%',
+        paddingTop: '2%',
+    },
+    underline: {
+        borderBottom: 'solid 2px #000000',
+        display: 'inline',
+        paddingBottom: 3
+    },
+    padding: {
+        paddingTop: 30
     }
 });
 
@@ -27,7 +54,7 @@ class Index extends Component {
         return (
             <Fragment>
                 <Head>
-                    <title>My New Portfolio</title>
+                    <title>Andrew Huynh</title>
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 </Head>
@@ -35,6 +62,31 @@ class Index extends Component {
                     <div className={classes.introText}>
                         <Typography variant='h5' color="inherit" align="center">Hello!</Typography>
                         <Typography variant='h2' color="inherit" align="center">Welcome to my new website.</Typography>
+                    </div>
+                    <div className={classes.introText2}>
+                        <Typography variant='h4' color="inherit" align="center">I'm</Typography>
+                        <Typography variant='h2' color="inherit" align="center">Andrew Huynh</Typography>
+                    </div>
+                </section>
+                <section className={classes.section}>
+                    <div className={classes.sectionText}>
+                        <Typography variant='h1' color="inherit" className={classes.underline}>Sections</Typography>
+                    </div>
+                    <NavButtons />
+                </section>
+                <section className={classes.section}>
+                    <div className={classes.sectionText}>
+                        <Typography variant='h1' color="inherit" className={classes.underline}>About</Typography>
+                    </div>
+                </section>
+                <section className={classes.section}>
+                    <div className={classes.sectionText}>
+                        <Typography variant='h1' color="inherit" className={classes.underline}>Code</Typography>
+                    </div>
+                </section>
+                <section className={classes.section}>
+                    <div className={classes.sectionText}>
+                        <Typography variant='h1' color="inherit" className={classes.underline}>Piano</Typography>
                     </div>
                 </section>
             </Fragment>
